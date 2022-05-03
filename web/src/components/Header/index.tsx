dd046f83053a514966e5552318b0eda753824eb9
glaubermatos <glaub.oliveira@hotmail.com>
2022-05-03 17:33:11 -0300

    created hydrometer daily reading page

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,15 +1,19 @@
 import { Box, Flex, Icon, IconButton, Text, Image } from "@chakra-ui/react";
 import { FiArrowLeft } from 'react-icons/fi'
 
-import logoImg from '/public/assets/logo.svg'
+import logoImg from '../../assets/logo.svg'
 
-export function Header() {
+interface HeaderProps {
+    title: string;
+}
+
+export function Header({title}: HeaderProps) {
     return(
-        <Box w={'100vw'} bg={'white'} borderBottom={'1px'} borderColor={'stroke'}>
+        <Box as="header" w={'100vw'} bg={'white'} borderBottom={'1px'} borderColor={'stroke'}>
             <Flex px='6' width={'100%'} margin={'auto'} maxWidth={'1120px'} as="header" justify={'space-between'} align={'center'} py="6">
                 <Image src={logoImg} alt="Logo watermonitor" />
                     
-                <Text>LEITURA DIÁRIA DO HIDRÔMETRO</Text>
+                <Text>{title}</Text>
 
                 <IconButton aria-label="Voltar para a página anterior" bg={'white'} borderWidth={1} borderColor={'stroke'} icon={<Icon as={FiArrowLeft}  />} />
             </Flex>
--- web/src/styles/theme.ts
+++ web/src/styles/theme.ts
@@ -5,6 +5,12 @@ const theme = extendTheme({
         background: "#F9FAFA",
         texts: '#455447',
         stroke: '#D9D9D9',
+        brand: {
+          blue: '#1FB3F5'
+        },
+        red: '#E05257',
+        redAlpha: 'RGBA(237, 28, 36, 0.05)',
+        redAlpha200: 'RGBA(237, 28, 36, 0.20)'
     },
     styles: {
       global: {
