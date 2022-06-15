d2ae1269e838eba517d0020d53a47c459f344da0
glaubermatos <glaub.oliveira@hotmail.com>
2022-06-15 15:35:22 -0300

    search for tanks

--- web/src/components/Tanks/index.tsx
+++ web/src/components/Tanks/index.tsx
@@ -12,7 +12,7 @@ interface TanksProps {
 
 export function Tanks( { tanks }: TanksProps) {
     return(
-        <Flex mt='12' gap='8' flexWrap='wrap' justify='center'>
+        <Flex mt='12' gap='8' flexWrap='wrap' justify={['center', 'center', 'flex-start']}>
             {tanks.map(tank => {
                 return(
                     <TankCard tank={tank} key={tank.id} />
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,9 +1,8 @@
 import { Link } from 'react-router-dom'
-import { Box, Container, Flex, Icon, IconButton, Link as ChakraLink, Text } from "@chakra-ui/react";
-import { FiLogOut } from 'react-icons/fi'
+import { Container, Flex, Input, InputGroup, InputRightElement, Link as ChakraLink, Stack, Text } from "@chakra-ui/react";
+import { FiSearch } from 'react-icons/fi'
 
 import { Header } from '../components/Header';
-import { TankCard } from '../components/TankCard';
 import { useEffect, useState } from 'react';
 import { api } from '../services/api';
 import { Tanks } from '../components/Tanks';
@@ -16,15 +15,22 @@ export interface Tank {
 export function Home() {
 
     const [tanks, setTanks] = useState<Tank[]>([])
+    const [tanksFilter, setTanksFilter] = useState<Tank[]>([])
 
     useEffect(() => {
         async function loadData() {
             const response = await api.get('/tanks')
             setTanks(response.data.tanks)
+            setTanksFilter(response.data.tanks)
         }
 
         loadData()
     }, [])
+
+    function handleFilterTank(searchValue: string) {
+        const tanksFiltered = tanks.filter(tank => tank.name.toLowerCase().includes(searchValue.toLowerCase()))
+        tanksFiltered ? setTanksFilter([...tanksFiltered]) : setTanksFilter(tanks)
+    }
 
     return(
         <>
@@ -46,8 +52,32 @@ export function Home() {
             </Flex>
         </Header>
         <Container maxWidth={'1120px'} p={'0'}>
-            <Tanks tanks={tanks} />
-            
+            <Flex mt='10' justify={['space-between']} direction={['column', 'row']} gap='5'>
+                <Stack spacing={1}>
+                    <Text fontSize={'1.375rem'} fontWeight='bold' lineHeight={'1.625rem'}>{tanks.length}</Text>
+                    <Text fontSize={'0.9375rem'} fontWeight='semibold' opacity={0.7}>Reservatórios</Text>
+                </Stack>
+                <InputGroup maxW={'22rem'}>
+                    <Input 
+                        type='search' 
+                        h='2.875rem' 
+                        placeholder='Pesquisar reservatório' 
+                        bg={'white'} 
+                        _hover={{bg: 'white'}}
+                        _focus={{bg: 'white'}}
+                        border={'1px'} 
+                        borderColor={'stroke'} 
+                        boxShadow='md' 
+                        onChange={(e) => handleFilterTank(e.target.value)}
+                    />
+                    <InputRightElement
+                        mt={'1'}
+                        pointerEvents='none'
+                        children={<FiSearch color='texts' size={'1.5rem'}/>}
+                    />
+                </InputGroup>
+            </Flex>
+            <Tanks tanks={tanksFilter} />            
         </Container>
         </>
     )
